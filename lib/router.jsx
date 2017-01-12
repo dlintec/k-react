ab424e5488a40959e7f9930ca1955701bb44a3d7
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2017-01-12 12:06:24 -0600

    router.jsx

---
+++ lib/router.jsx
@@ -0,0 +1,33 @@
+import React, { Component, PropTypes  } from 'react';
+import {mount} from 'react-mounter';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
+import reactApp from '/imports/ui/components/reactApp.jsx';
+// load Layout and Welcome React components
+
+main_route={
+  action: function(params) {
+    BlazeLayout.render("flow_layout",  { top:"header",main: "mainPage" })
+  }
+}
+FlowRouter.route('/1', main_route);
+FlowRouter.route('/', main_route);
+
+class MainLayoutCtx extends React.Component {
+   render(){
+    const {content} = this.props //destructuring obj
+    return (
+     <div>
+      {content}
+     </div>
+    )
+  }
+}
+FlowRouter.route("/react2", {
+  action() {
+    console.log("react2");
+
+    mount( MainLayoutCtx, {content: <reactApp/>});
+    //BlazeLayout.render("flow_layout",  { top:"header" })
+    //ReactDOM.render( <beerApp />, document.getElementById('render-target') );
+  }
+});
