1a07084fb6ab84874c9c4e6fc09ccc5bda8db6b6
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-22 19:51:46 -0500

    Update mobile-config.js

--- mobile-config.js
+++ mobile-config.js
@@ -1,5 +1,7 @@
 // inside mobile-config.js
 App.info({
-  version: '1.0.0',
+  name: 'My App',
+  description: 'An Android app built with Meteor',
+  version: '0.0.1',
   buildNumber: '101' // has to be a number
 })
